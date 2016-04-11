d248fdc30cf6c714348bfb1a2ebd2d97de604753
Silviu G. Niculita <sniculita@riasolutionsgroup.com>
2016-04-11 11:17:13 +0300

    Initial commit.

---
+++ BindableScreen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using PNet.Icap.Platform.ControlSdk2;
+
+namespace BindingEngine
+{
+    public class BindableScreen<TViewModel> : PNetPage where TViewModel : BindableObject
+    {
+        public int ResourceId { get; set; }
+
+        public TViewModel DataContext { get; set; }
+
+        public int ViewLayoutResourceId { get; set; }
+
+        protected BindableScreen(TViewModel viewModel, int viewLayoutResourceId)
+        {
+            this.ViewLayoutResourceId = viewLayoutResourceId;
+
+            this.DataContext = viewModel;
+        }
+
+        public override void OnStart()
+        {
+            base.OnStart();
+            BindingEngine.Initialize(this);
+        }
+
+    }
+}
---
+++ Converters/SpinnerToObjectConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+
+namespace BindingEngine.Converters
+{
+    [Android.Runtime.Preserve]
+    public class SpinnerToObjectConverter : IBindingValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (IList)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(string))
+            {
+                var propertyInfo = value.GetType().GetProperty("Instance");
+                return propertyInfo == null ? null : propertyInfo.GetValue(value, null);
+            }
+            return value.ToString();
+        }
+    }
+}
